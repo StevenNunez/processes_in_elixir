b26411342beb7c614594237782d3c144884cc1b7
Steven Nunez <steven.nunez@gmail.com>
2016-04-26 18:04:51 -0400

    Solution

--- lib/processes_in_elixir.ex
+++ lib/processes_in_elixir.ex
@@ -1,2 +1,12 @@
 defmodule ProcessesInElixir do
+  def loop do
+    receive do
+      {:add, num1, num2, requester} ->
+        send requester, {:total, num1 + num2}
+      {:reverse, value, requester} ->
+        send requester, {:result, String.reverse(value)}
+    end
+
+    loop
+  end
 end
